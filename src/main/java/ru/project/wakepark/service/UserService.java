4a439d4af1a29d4919d3ae060fc037420fc1c180
vladimirvolkov <angelrum@inbox.ru>
2020-06-23 00:29:06 +0300

    Настройки heroku

--- src/main/java/ru/project/wakepark/service/UserService.java
+++ src/main/java/ru/project/wakepark/service/UserService.java
@@ -1,10 +1,11 @@
 package ru.project.wakepark.service;
 
 import org.springframework.stereotype.Service;
+import ru.project.wakepark.model.User;
 import ru.project.wakepark.repository.CrudUserRepository;
 
 @Service
-public class UserService extends AbstractService {
+public class UserService extends AbstractService<User> {
 
     private CrudUserRepository repository;
 
