bb8a13ee0858e20b4b8586ca13b15169b4ab1903
vladimirvolkov <angelrum@inbox.ru>
2020-06-23 11:41:09 +0300

    Настройки heroku

--- hr.bat
+++
@@ -1,2 +0,0 @@
-call mvn -B -s settings.xml -DskipTests=true clean package
-call java -Dspring.profiles.active="heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/wakepark" -jar target/dependency/webapp-runner.jar target/*.war
