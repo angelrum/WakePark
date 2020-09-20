575dcb2983e24d2e55fa536e204e83bb2fb06360
vladimirvolkov <angelrum@inbox.ru>
2020-09-20 11:43:18 +0300

    webapp runner

--- src/main/java/ru/project/wakepark/WebSocketConfig.java
+++ src/main/java/ru/project/wakepark/WebSocketConfig.java
@@ -1,10 +1,17 @@
 package ru.project.wakepark;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import javax.net.ssl.SSLContext;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -23,6 +30,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         registry.addEndpoint("/chat");
         registry.addEndpoint("/chat").withSockJS();
+    }
+
+    @Bean
+    public StandardWebSocketClient getWebSocketClient() throws NoSuchAlgorithmException {
+        StandardWebSocketClient simpleWebSocketClient =
+                new StandardWebSocketClient();
+        Map<String, Object> userProperties = new HashMap<>();
+        userProperties.put("org.apache.tomcat.websocket.SSL_CONTEXT", SSLContext.getDefault());
+        simpleWebSocketClient.setUserProperties(userProperties);
+        return simpleWebSocketClient;
     }
 
 }
